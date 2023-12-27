b268e36d60e82a083d5bfedf9562f8e7b5cebeef
Surefire10 <109690531+Surefire10@users.noreply.github.com>
2023-12-27 17:57:11 +0200

    Changed content

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,13 +17,6 @@ function About(){
                         <p>
                             At 14, I decided to try my hand at coding for the first time and ever since then, 
                             I just knew that's what I was going to do with my life. 
-                            Fast-forward through 4 more years of school and I decided that 
-                            the only way for me was to pursue my bachelor's of Computer Science. 
-                            
-                        </p>
-                        <p>
-                             Today, I have the privilege of being in the field that 
-                            I've always dreamed of, working with amazing clients on exciting projects.
                         </p>
                         <p>
                         Fast-forward through 4 more years of school and I decided that 
@@ -40,19 +33,18 @@ function About(){
                 </div>
                 <div className={classes.skillContainer}>
                     <div className={classes.skillTitle}>
-                        <h2>Here's What I Know.</h2>
+                        <h2>Here's What I Know:</h2>
                     </div>
                     <div className={classes.skills}>
                         
                         <div><h2>Java</h2></div>
+                        <div><h2>JavaScript</h2></div>
+                        <div><h2>React</h2></div>
                         <div><h2>SpringBoot</h2></div>
                         <div><h2>SQL</h2></div>
                         <div><h2>HTML</h2></div>
                         <div><h2>CSS</h2></div>
-                        <div><h2>JavaScript</h2></div>
-                        <div><h2>Angular</h2></div>
-                        <div><h2>React</h2></div>
-
+                        <div><h2>TypeScrips</h2></div>
                     </div>
                 </div>
             </div>
